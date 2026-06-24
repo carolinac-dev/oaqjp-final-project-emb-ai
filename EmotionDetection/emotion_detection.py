97d254c2e08a40b6981af76bf9183fafc6af82d3
CarolinaC <carolinacalvocabrera@gmail.com>
2026-06-24 10:20:54 -0400

    Tasks 6 and 7

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,6 +1,17 @@
 import requests, json
 
 def emotion_detector(text_to_analyze):
+    # Blank itext
+    if text_to_analyze is None or text_to_analyze.strip() == "":
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+
     # Define a function that takes a string input.
     # URL
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
@@ -10,8 +21,48 @@ def emotion_detector(text_to_analyze):
     myobj = { "raw_document": { "text": text_to_analyze } }
     # Response.
     response = requests.post(url, json = myobj, headers=headers)
-    formatted_response = json.loads(response.text)
+
+    # Error.
+    if response.status_code == 400:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+    
+    # Correct answer.
+    try:
+        formatted_response = json.loads(response.text)
+    except Exception:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+
+    if (
+        not isinstance(formatted_response, dict) or
+        'emotionPredictions' not in formatted_response or
+        not formatted_response['emotionPredictions'] or
+        len(formatted_response['emotionPredictions']) == 0
+    ):
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+
     emotions = formatted_response['emotionPredictions'][0]['emotion']
+
     dominant_emotion = max(emotions, key = emotions.get)
     return {
         'anger': emotions['anger'],
